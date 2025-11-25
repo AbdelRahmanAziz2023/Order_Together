b26ebd78409bcdeebf3397a031239b844c0739eb
mohamed-khaled-xv <mohamedkhaledxv8@gmail.com>
2025-11-25 16:05:51 +0200

    Cart redux state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import baseApi from "../services/api/baseApi";
+import cartReducer from "./slices/cartSlice";
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
+    cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
