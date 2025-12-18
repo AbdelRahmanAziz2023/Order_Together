0ff13e7f055640c4afd0decc5e85f4e8c42ddb89
AbdelRahmanAziz2023 <baljdawy@gmail.com>
2025-12-18 10:59:38 +0200

    delete some images

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,93 +1,52 @@
-import Error from '../../assets/images/error.svg';
-import ActiveHome from '../../assets/images/icons/ActiveHome.svg';
-import ActiveUser from '../../assets/images/icons/ActiveUser.svg';
-import Arrow from '../../assets/images/icons/Arrow.svg';
-import AuthBack from '../../assets/images/icons/AuthBack.svg';
-import Back from '../../assets/images/icons/back.svg';
-import Bell from '../../assets/images/icons/bell.svg';
-import Call from '../../assets/images/icons/Call.svg';
-import Camera from '../../assets/images/icons/Camera.svg';
-import Car from '../../assets/images/icons/Car.svg';
-import Cart from '../../assets/images/icons/Cart.svg';
-import Check from '../../assets/images/icons/Check.svg';
-import Clock from '../../assets/images/icons/Clock.svg';
-import Cross from '../../assets/images/icons/Close.svg';
-import CreditCard from '../../assets/images/icons/credit-card.svg';
-import Delivery from '../../assets/images/icons/Delivery.svg';
-import EllipseMustard from '../../assets/images/icons/EllipseMusterd.svg';
-import EllipseRed from '../../assets/images/icons/EllipseRed.svg';
-import EyeOff from '../../assets/images/icons/eye-off.svg';
-import Eye from '../../assets/images/icons/eye.svg';
-import Facebook from '../../assets/images/icons/facebook.svg';
-import Filter from '../../assets/images/icons/Filter.svg';
-import Google from '../../assets/images/icons/google.svg';
-import Heart from '../../assets/images/icons/heart.svg';
-import Home from '../../assets/images/icons/Home.svg';
-import Info from '../../assets/images/icons/info.svg';
-import Logout from '../../assets/images/icons/Logout.svg';
-import Menu from '../../assets/images/icons/Menu.svg';
-import Message from '../../assets/images/icons/Message.svg';
-import MicOff from '../../assets/images/icons/mic-off.svg';
-import Minus from '../../assets/images/icons/Minus.svg';
-import More from '../../assets/images/icons/More.svg';
-import Plus from '../../assets/images/icons/Plus.svg';
-import Polygon from '../../assets/images/icons/Polygon.svg';
-import Question from '../../assets/images/icons/Question.svg';
-import Search from '../../assets/images/icons/Search.svg';
-import Send from '../../assets/images/icons/Send.svg';
-import Smile from '../../assets/images/icons/smile.svg';
-import Star from '../../assets/images/icons/Star.svg';
-import Tag from '../../assets/images/icons/Tag.svg';
-import UnderArrow from '../../assets/images/icons/Under Arrow.svg';
-import User from '../../assets/images/icons/User.svg';
-import Volume from '../../assets/images/icons/volume-1.svg';
-import Watch from '../../assets/images/icons/Watch.svg';
-import LogoIcon from '../../assets/images/LogoIcon.svg';
-
+import Error from "../../assets/images/error.svg";
+import ActiveHome from "../../assets/images/icons/ActiveHome.svg";
+import ActiveUser from "../../assets/images/icons/ActiveUser.svg";
+import Arrow from "../../assets/images/icons/Arrow.svg";
+import AuthBack from "../../assets/images/icons/AuthBack.svg";
+import Back from "../../assets/images/icons/back.svg";
+import Bell from "../../assets/images/icons/bell.svg";
+import Camera from "../../assets/images/icons/Camera.svg";
+import Cart from "../../assets/images/icons/Cart.svg";
+import Check from "../../assets/images/icons/Check.svg";
+import Clock from "../../assets/images/icons/Clock.svg";
+import Cross from "../../assets/images/icons/Close.svg";
+import CreditCard from "../../assets/images/icons/credit-card.svg";
+import Delivery from "../../assets/images/icons/Delivery.svg";
+import EllipseMustard from "../../assets/images/icons/EllipseMusterd.svg";
+import EllipseRed from "../../assets/images/icons/EllipseRed.svg";
+import EyeOff from "../../assets/images/icons/eye-off.svg";
+import Eye from "../../assets/images/icons/eye.svg";
+import Home from "../../assets/images/icons/Home.svg";
+import Logout from "../../assets/images/icons/Logout.svg";
+import Minus from "../../assets/images/icons/Minus.svg";
+import Plus from "../../assets/images/icons/Plus.svg";
+import User from "../../assets/images/icons/User.svg";
+import Watch from "../../assets/images/icons/Watch.svg";
+import LogoIcon from "../../assets/images/LogoIcon.svg";
 
 export const Icons = {
   back: Back,
   bell: Bell,
   user: User,
   activeUser: ActiveUser,
-  search: Search,
   home: Home,
   activeHome: ActiveHome,
-  heart: Heart,
   watch: Watch,
-  underArrow: UnderArrow,
   plus: Plus,
   minus: Minus,
-  question: Question,
-  send: Send,
-  tag: Tag,
-  more: More,
   camera: Camera,
   cross: Cross,
-  filter: Filter,
   eye: Eye,
   eyeOff: EyeOff,
-  info: Info,
   creditCard: CreditCard,
   check: Check,
-  car: Car,
-  call: Call,
   logout: Logout,
-  message: Message,
-  micOff: MicOff,
-  polygon: Polygon,
-  smile: Smile,
-  star: Star,
-  volume: Volume,
   authBack: AuthBack,
   ellipseRed: EllipseRed,
   ellipseMustard: EllipseMustard,
-  google: Google,
-  facbook: Facebook,
   delivery: Delivery,
   clock: Clock,
   arrow: Arrow,
-  menu: Menu,
   cart: Cart,
 };
 
